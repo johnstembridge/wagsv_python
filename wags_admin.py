f97b1086584e8fd7024e9762e91af2c2c9d50818
johnstembridge <john.stembridge@gmail.com>
2018-01-28 01:24:52 +0000

    set redirect for login

--- wags_admin.py
+++ wags_admin.py
@@ -17,7 +17,7 @@ from models.user import User
 app = Flask(__name__)
 
 login_manager = LoginManager(app)
-login_manager.login_view = config.url_for_admin('login')
+login_manager.login_view = config.get('url_prefix')['admin']+'/login'
 
 app.config['SECRET_KEY'] = config.get('SECRET_KEY')
 csrf = CSRFProtect(app)
