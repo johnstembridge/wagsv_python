43f44aef9bf7a8539803bf52689468945954ccd8
johnstembridge <john.stembridge@gmail.com>
2018-03-18 18:35:08 +0000

    show handicap history

--- front_end/user/handicap_history_form.py
+++ front_end/user/handicap_history_form.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FormField, FieldList
 from wtforms.fields.html5 import DateField
-from back_end.interface import get_player_name, get_handicap_history
+from back_end.interface import get_player_name, get_player_id, get_handicap_history
+from data_utilities import is_num, normalise_name
 
 
 class HandicapItemForm(FlaskForm):
@@ -15,6 +16,8 @@ class HandicapHistoryForm(FlaskForm):
     history = FieldList(FormField(HandicapItemForm))
 
     def populate_history(self, player_id, date):
+        if not is_num(player_id):
+            player_id = get_player_id(normalise_name(player_id))
         self.player.data = get_player_name(player_id)
         for item in get_handicap_history(player_id, date):
             item_form = HandicapItemForm()
