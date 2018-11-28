ee5cf032498432323b635d03225e6bc5289c0ef8
johnstembridge <john.stembridge@gmail.com>
2018-11-28 19:11:16 +0000

    test sending email

--- globals/email.py
+++ globals/email.py
@@ -1,15 +1,14 @@
 from flask import current_app
-from flask_sendmail import Message, Mail
+from flask_sendmail import Message
 
 from globals import config
 from globals.decorators import async
 from back_end.data_utilities import force_list
-# from globals.app_setup import mail
+from globals.app_setup import mail
 
 
 @async
 def send_async_email(app, msg):
-    mail = Mail(app)
     with app.app_context():
         mail.send(msg)
 
@@ -21,7 +20,6 @@ def send_mail(to, sender, cc=None, subject=None, message=None):
     msg.cc = force_list(cc)
     msg.body = '\n'.join(message) if type(message) is list else message
     app = current_app._get_current_object()
-    mail = Mail(app)
     if config.get('send_mail'):
         send_async_email(app, msg)
         #mail.send(msg)
