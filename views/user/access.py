3a8061933785403541e07f5e024c9a3c8f203706
jstembridge <john.stembridge@gmail.com>
2019-01-22 17:20:24 +0000

    redirects to use https

--- views/user/access.py
+++ views/user/access.py
@@ -8,8 +8,7 @@ from back_end.interface import get_user
 
 role = 'user'
 login_manager = LoginManager(app)
-with app.app_context():
-    login_endpoint = config.url_for_app(role, 'user_login')
+login_endpoint = config. full_url_for_app(role, 'login')
 login_manager.login_view = login_endpoint
 app.logger.info('Login View: {}'.format(login_manager.login_view))
 
