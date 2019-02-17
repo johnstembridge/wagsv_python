9555ab3e73756d7bd1a3029e17de88e3a0a8c765
johnstembridge <john.stembridge@gmail.com>
2019-02-17 12:05:40 +0000

    Try to make login response https

--- views/user/access.py
+++ views/user/access.py
@@ -7,7 +7,7 @@ from back_end.interface import get_user
 
 role = 'user'
 login_manager = LoginManager(app)
-login_manager.login_view = config.get('url_prefix')[role]+'/login'
+login_manager.login_view = config.get('locations')['base_url'] + config.get('url_prefix')[role][1:] + '/login'
 login_manager.login_message = 'You must be a WAGS member to access this page'
 
 
