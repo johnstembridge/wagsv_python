23a4959e9a8d05c94c26692630810a690816be10
johnstembridge <john.stembridge@gmail.com>
2019-02-17 13:57:41 +0000

    Try to make login response https

--- front_end/login.py
+++ front_end/login.py
@@ -21,31 +21,31 @@ class LoginForm(FlaskForm):
         pass
 
 
-def user_login(wagsapp, next_page, app=None):
+def user_login(wags_app, next_page, app=None):
     if current_user.is_authenticated:
         app.logger.info('next page: ' + next_page)
-        app.logger.info('qualified next page: ' + qualify_url(wagsapp, next_page))
-        return redirect(qualify_url(wagsapp, next_page))
+        app.logger.info('qualified next page: ' + qualify_url(wags_app, next_page))
+        return redirect(qualify_url(wags_app, next_page))
     form = LoginForm()
     if form.is_submitted():
         if form.validate_on_submit():
             user = get_user(user_name=form.username.data)
             if user is None or not user.check_password(form.password.data):
                 flash('Invalid username or password', 'danger')
-                return render_template('{}/login.html'.format(wagsapp), title='Sign In', form=form, wagsapp=wagsapp, url_for_app=url_for_app)
-            # if wagsapp not in [role.role.name for role in user.roles]:
-            #     flash('Sorry, you do not have {} access'.format(wagsapp))
-            #     return redirect(qualify_url(wagsapp))
+                return render_template('{}/login.html'.format(wags_app), title='Sign In', form=form, wags_app=wags_app, url_for_app=url_for_app)
+            # if wags_app not in [role.role.name for role in user.roles]:
+            #     flash('Sorry, you do not have {} access'.format(wags_app))
+            #     return redirect(qualify_url(wags_app))
             login_user(user, remember=form.remember_me.data)
             if not next_page or url_parse(next_page).netloc != '':
-                next_page = qualify_url(wagsapp)
+                next_page = qualify_url(wags_app)
             else:
-                next_page = qualify_url(wagsapp, next_page)
+                next_page = qualify_url(wags_app, next_page)
             return redirect(next_page)
     else:
         form.populate()
 
-    return render_template('{}/login.html'.format(wagsapp), title='Sign In', form=form, wagsapp=wagsapp,  url_for_app=url_for_app)
+    return render_template('{}/login.html'.format(wags_app), title='Sign In', form=form, wags_app=wags_app,  url_for_app=url_for_app)
 
 
 class RegistrationForm(FlaskForm):
@@ -62,9 +62,9 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Please use a different username.')
 
 
-def user_register(wagsapp):
+def user_register(wags_app):
     if current_user.is_authenticated:
-        return redirect(qualify_url(wagsapp))
+        return redirect(qualify_url(wags_app))
     form = RegistrationForm()
     if form.is_submitted():
         if form.validate_on_submit():
@@ -72,26 +72,26 @@ def user_register(wagsapp):
             if member:
                 if member.status not in [MemberStatus.full_member, MemberStatus.overseas_member]:
                     flash('Sorry, you are not a current member', 'danger')
-                    return redirect(qualify_url(wagsapp))
+                    return redirect(qualify_url(wags_app))
                 user = User(user_name=form.username.data, member_id=member.id)
                 user.set_password(form.password.data)
-                if wagsapp == 'user':
+                if wags_app == 'user':
                     role = Role(role=UserRole.user)
-                if wagsapp == 'admin':
+                if wags_app == 'admin':
                     role = Role(role=UserRole.admin)
                 user.roles.append(role)
                 save_user(user)
-                flash('Congratulations, you are now a registered {}!'.format(wagsapp), 'success')
-                return redirect(url_for_app(wagsapp, 'user_login'))
+                flash('Congratulations, you are now a registered {}!'.format(wags_app), 'success')
+                return redirect(url_for_app(wags_app, 'user_login'))
             else:
                 flash('Cannot find your membership - please give your WAGS contact email address')
-    return render_template('{}/register.html'.format(wagsapp), title='Register', form=form)
+    return render_template('{}/register.html'.format(wags_app), title='Register', form=form)
 
 
-def qualify_url(wagsapp, page=None):
-    return (url_for_app(wagsapp, 'index') + (page or ''))
+def qualify_url(wags_app, page=None):
+    return (url_for_app(wags_app, 'index') + (page or ''))
 
 
-def user_logout(wagsapp):
+def user_logout(wags_app):
     logout_user()
-    return redirect(qualify_url(wagsapp))
+    return redirect(qualify_url(wags_app))
