ea703930a1082be6c179ef0b64b8bec1114f971f
jstembridge <john.stembridge@gmail.com>
2019-03-15 17:23:45 +0000

    Show minutes

---
+++ front_end/user/minutes.py
@@ -0,0 +1,43 @@
+from flask import render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, SelectField, HiddenField
+
+from globals.enumerations import MinutesType
+from back_end.data_utilities import fmt_date, parse_date
+from back_end.interface import get_all_years
+from front_end.form_helpers import render_link
+from globals.config import url_for_user
+from models.minutes import Minutes
+
+
+class MinutesShow:
+    @staticmethod
+    def minutes_show():
+        form = MinutesShowForm()
+        if form.is_submitted():
+            minutes = form.populate()
+            if minutes:
+                return redirect(minutes.file_link())
+        return render_template('user/minutes_show.html', form=form, render_link=render_link, url_for_user=url_for_user)
+
+
+class MinutesShowForm(FlaskForm):
+    minutes_type = SelectField(label='Meeting type', choices=MinutesType.choices(), coerce=MinutesType.coerce)
+    minutes_year = SelectField(label='Year', choices=[(str(y), str(y)) for y in get_all_years()])
+    minutes = SelectField(label='Minutes', choices=[])
+    select = SubmitField(label='Select')
+    type_year = HiddenField()
+
+    def populate(self):
+        type_year = self.minutes_type.data.name + '_' + self.minutes_year.data
+        if type_year != self.type_year.data:
+            minutes = Minutes.list_all_minutes(self.minutes_type.data, int(self.minutes_year.data))
+            if len(minutes) == 1:
+                return minutes[0]
+            choices = [fmt_date(m.date) for m in minutes]
+            self.minutes.choices = list(zip(choices, choices))
+        if self.minutes.data is not None and self.minutes.data != 'None' and type_year == self.type_year.data:
+            return Minutes(self.minutes_type.data, parse_date(self.minutes.data))
+        else:
+            self.type_year.data = type_year
+            return None
---
+++ models/minutes.py
@@ -0,0 +1,74 @@
+import os
+from globals import config
+from globals.config import url_for_html
+from back_end.data_utilities import fmt_date, parse_date, force_list
+from globals.enumerations import MinutesType
+
+minutes_location = config.get('locations')['minutes']
+
+
+class Minutes:
+
+    def __init__(self, type=None, date=None, file_type='pdf'):
+        self.type = type
+        self.date = date
+        self.file_type = file_type
+
+    def file_name(self):
+        meeting_date = fmt_date(self.date).replace('/', ' ')
+        meeting_type = Minutes.map_type_to_file(self.type)
+        return meeting_type + ' ' + meeting_date + '.' + self.file_type
+
+    def file_link(self, draft=False):
+        draft = 'drafts' if draft else ''
+        return url_for_html('minutes', draft, self.file_name())
+
+    def full_type(self):
+        if self.type == MinutesType.Committee:
+            return 'Committee meeting'
+        if self.type == MinutesType.AGM:
+            return 'AGM'
+
+    @staticmethod
+    def file_path(draft=False):
+        draft = 'drafts' if draft else ''
+        return os.path.join(minutes_location, draft)
+
+    @staticmethod
+    def list_all_minutes(type=None, year=None):
+        if not type:
+            type = [t for t in MinutesType]
+        type = [Minutes.map_type_to_file(t) for t in force_list(type)]
+        files = os.listdir(minutes_location)
+        minutes = [
+            Minutes(Minutes.map_file_to_type(f[:3]), parse_date(f[-14:-4], ' '), f[-3:])
+            for f in files
+            if not os.path.isdir(f) and f[:3] in type
+        ]
+        if year:
+            minutes = [m for m in minutes if m.date.year in force_list(year)]
+        return minutes
+
+    @staticmethod
+    def latest_minutes():
+        minutes = Minutes.list_all_minutes()
+        latest_date = max(m.date for m in minutes)
+        latest = [m for m in minutes if m.date == latest_date][0]
+        return latest
+
+    @staticmethod
+    def map_type_to_file(type):
+        if type == MinutesType.Committee:
+            return 'min'
+        if type == MinutesType.AGM:
+            return 'agm'
+
+    @staticmethod
+    def map_file_to_type(file):
+        if file == 'min':
+            return MinutesType.Committee
+        if file == 'agm':
+            return MinutesType.AGM
+
+
+
