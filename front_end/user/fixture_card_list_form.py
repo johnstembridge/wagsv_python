6369a1891090be552cf2fe33ea46bae5322e77f8
johnstembridge <john.stembridge@gmail.com>
2025-08-16 19:21:42 +0100

    Fixture Cards

--- front_end/user/fixture_card_list_form.py
+++ front_end/user/fixture_card_list_form.py
@@ -21,5 +21,5 @@ class FixtureCardListForm(FlaskForm):
             year = file[-8:][:4]
             item_form.year.data = year
             item_form.image_url.data = url_for_html('pictures', 'fixture_cards', file)
-            item_form.new_row = count %3 == 0
+            item_form.new_row = "y" if count %3 == 0 else "n"
             self.card_list.append_entry(item_form)
