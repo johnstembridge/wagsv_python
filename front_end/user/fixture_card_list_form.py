8bba8dca3417727e9475af7a2faea8f1241725c6
johnstembridge <john.stembridge@gmail.com>
2025-08-16 19:13:38 +0100

    Fixture Cards

--- front_end/user/fixture_card_list_form.py
+++ front_end/user/fixture_card_list_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FieldList, FormField
+from wtforms import StringField, FieldList, FormField, HiddenField
 from globals import config
 from globals.config import url_for_html
 import os
@@ -7,6 +7,7 @@ import os
 class FixtureCardItemForm(FlaskForm):
     year = StringField()
     image_url = StringField()
+    new_row = HiddenField()
 
 class FixtureCardListForm(FlaskForm):
     card_list = FieldList(FormField(FixtureCardItemForm))
@@ -14,9 +15,11 @@ class FixtureCardListForm(FlaskForm):
     def populate_card_list(self):
         path = os.path.join(config.get('locations')['pictures'], 'fixture_cards')
         files = sorted(os.listdir(path), reverse=True)
+        count = 0
         for file in files:
             item_form = FixtureCardItemForm()
             year = file[-8:][:4]
             item_form.year.data = year
             item_form.image_url.data = url_for_html('pictures', 'fixture_cards', file)
+            item_form.new_row = count %3 == 0
             self.card_list.append_entry(item_form)
