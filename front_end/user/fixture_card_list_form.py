b60b1cdbf20f16ea8fdbf7e47563d2f047049599
johnstembridge <john.stembridge@gmail.com>
2025-08-18 18:24:47 +0100

    Fixture Cards

--- front_end/user/fixture_card_list_form.py
+++ front_end/user/fixture_card_list_form.py
@@ -18,13 +18,13 @@ class FixtureCardListForm(FlaskForm):
         path = os.path.join(config.get('locations')['pictures'], 'fixture_cards')
         files = sorted(os.listdir(path), reverse=True)
         count = 0
-        for file in files:
+        for file in files[1:]:
             item_form = FixtureCardItemForm()
             year = file[-8:][:4]
             item_form.year.data = year
             item_form.thumb_url.data = url_for_html('pictures', 'fixture_cards', 'thumbs', file)
             item_form.image_url.data = url_for_html('pictures', 'fixture_cards', file)
             item_form.start_row = count%4 == 0
-            item_form.end_row = (count+1)%4 == 0 or count == len(files)-1
+            item_form.end_row = (count+1)%4 == 0 or count == len(files)-2
             count += 1
             self.card_list.append_entry(item_form)
