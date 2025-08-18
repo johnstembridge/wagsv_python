19c90d3619dd83bd962a4278641736d82191d347
johnstembridge <john.stembridge@gmail.com>
2025-08-18 18:10:10 +0100

    Fixture Cards

--- front_end/user/fixture_card_list_form.py
+++ front_end/user/fixture_card_list_form.py
@@ -6,6 +6,7 @@ import os
 
 class FixtureCardItemForm(FlaskForm):
     year = StringField()
+    thumb_url = StringField()
     image_url = StringField()
     start_row = HiddenField()
     end_row = HiddenField()
@@ -21,6 +22,7 @@ class FixtureCardListForm(FlaskForm):
             item_form = FixtureCardItemForm()
             year = file[-8:][:4]
             item_form.year.data = year
+            item_form.thumb_url.data = url_for_html('pictures', 'fixture_cards', 'thumbs', file)
             item_form.image_url.data = url_for_html('pictures', 'fixture_cards', file)
             item_form.start_row = count%4 == 0
             item_form.end_row = (count+1)%4 == 0 or count == len(files)-1
